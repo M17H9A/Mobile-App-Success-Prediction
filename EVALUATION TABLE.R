4b1e90170c55b7bce51a9f4a5450020971253664
Mithra Anand <99313878+M17H9A@users.noreply.github.com>
2026-07-24 22:22:52 +0530

    Add files via upload

---
+++ EVALUATION TABLE.R
@@ -0,0 +1,10 @@
+# Create an empty dataframe to store results
+results_df <- data.frame(
+  Model = character(),
+  Accuracy = character(),
+  Precision = numeric(),
+  Recall = numeric(),
+  F1_Score = numeric(),
+  AUC_ROC = numeric(),
+  stringsAsFactors = FALSE
+)
